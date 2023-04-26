a4f8cae51efde3c439dec4d170b467be003a0366
tom-2810 <tom.poortman@hva.nl>
2023-04-26 12:25:03 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from 'express'
+
+// Maak een nieuwe express app
+const app = express()
+
+// Stel in hoe we express gebruiken
+app.set('view engine', 'ejs')
+app.set('views', './views')
+app.use(express.static('public'))
+
+// Stel afhandeling van formulieren in
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// Maak een route voor de index
+app.get('/', (request, response) => {
+    response.render('index', {active: '/' })
+})
+
+// Stel het poortnummer in en start express
+app.set('port', process.env.PORT || 8000)
+app.listen(app.get('port'), function () {
+  console.log(`Application started on http://localhost:${app.get('port')}`)
+})
